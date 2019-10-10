f4305bb707a7c5b428fc13b25a0911e78f9bd865
Jardel Kuhn - SAMSUNG <jardelkuhn@gmail.com>
2019-10-09 23:53:46 -0300

    feature: initialized frontend app. 1. Configured redux, store, saga and initial settings. 2. Added signup, signin structure

---
+++ frontend/src/pages/SignUp/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import { useDispatch } from 'react-redux';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+// import { Container } from './styles';
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <img src={null} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
+}
